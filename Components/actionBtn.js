44b6d1049a527dfba600615f7b2c397b48473992
getulio.souza <getulio.souza@evolutix.com.br>
2023-06-22 15:32:05 -0300

    home-page-done

---
+++ Components/actionBtn.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+const ActionBtn = props => {
+  const content = (
+    <View style={[styles.button, { backgroundColor: props.color }]}>
+      <Text style={styles.text}>{props.text}</Text>
+    </View>
+  )
+  return <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 148,
+    height: 75,
+    color: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight:'700'
+  }
+})
+
+export default ActionBtn
--- Pages/Home.js
+++ Pages/Home.js
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import ActionBtn from '../Components/actionBtn'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.backgroundContainer}>
+      {/* header */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Seja bem vindo ao BeDev!</Text>
+        <Text>Estamos aqui para te desenvolver profissionalmente e te alocar no mercado de trabalho, e o melhor: de forma gratuita</Text>
+      </View>
+      {/* logo */}
+      <View>
+        <Image
+          style={{width: 300, height: 300}}
+          source={require('../assets/photo-1.jpeg')}
+        />
+      </View>
+      {/* buttons */}
+      <View style={styles.btnsContainer}>
+        <TouchableOpacity>
+          <ActionBtn
+            text="quero estudar"
+            color='#390072'
+            
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ActionBtn
+            text="quero trabalhar"
+            color="#390072"
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    backgroundColor:'#fff',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  header: {
+    paddingLeft: 20,
+    paddingRight:20
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: '700',
+    paddingBottom:20
+  },
+
+  logo: {
+    flex:1
+  },
+
+  btnsContainer: {
+    flexDirection: 'row',
+    gap:20
+  }
+
+})
+
 
 export default Home
